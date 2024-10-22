c4f4893ea712446cbda10a79acc325bf2b659115
OscarGRubianoA <oscargio86@hotmail.com>
2024-10-21 23:24:36 -0500

    'src'

--- alura.js
+++ alura.js
@@ -66,7 +66,7 @@ function generarCombinaciones(array) {
   function majorCombination(array){
     let major = array[0];
     for(let i = 0; i < array.length; i++){
-        if(array[i]>major){
+        if(array[i] >major){
            major = array[i];
         }
     }
